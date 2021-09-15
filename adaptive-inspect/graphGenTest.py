864b2187c82e358b577181f9874731b178619825
tomwelsh <34302535+tomwelsh@users.noreply.github.com>
2021-09-15 07:56:32 +0100

    experimental setup

--- adaptive-inspect/graphGenTest.py
+++ adaptive-inspect/graphGenTest.py
@@ -1,52 +1,73 @@
 import modelTopology
 import adaptiveInspect
 import random
-
+import time
 
 #create combined topology
 
 
 
 #AInsp loop
-result=0
+#experiment parameters
+netSizes=[10,20,30,40,50]
+randomCosts=[True,False]
+valueAnalysis=[False,True]
+nodeTweak=[0.1,0.2,0.3,0.4]
+adj=[False,True]
+basecosts=0.1
+#costTweak=[0,1,2,3,4,5]
 runs=100
-for j in range(runs):
-    scT1=modelTopology.scTopology()
+avgs=open(("avgs%f" % time.time()),'w')
+dataSet=open("data%f"% time.time(),'w')
+dataSet.write("NetSize,RandomCosts,ValueAnalysis,NodeTweak,AdjacencyFlag,Results")
+for size in netSizes:
+    for rCosts in randomCosts:
+        for v in valueAnalysis:
+            for nTweak in nodeTweak:
+                    for aFlag in adj:
+                        dataSet.write("\n%d,%s,%s,%f,%s" % (size,rCosts,v,nTweak,aFlag))
+                        results=[]
+                        result=0
+                        for j in range(runs):
+                            scT1=modelTopology.scTopology()
+                            #generate a new graph
+                            scT1.scInterface.genLinear(size)
+                            #scT1.scInterface.drawGraph()
+                            #create a new adaptive inspection instance
+                            insp1=adaptiveInspect.inspect(scT1.scInterface)
+                            insp1.suspectNodes.append(random.randint(0,size-1))
+                            for i in range(size):
+                                insp1.costs[i]=basecosts
 
-    #generate a new graph
-    size=100
-    scT1.scInterface.genLinear(size)
-    #scT1.scInterface.drawGraph()
+                            if rCosts==True:
+                                insp1.randomCosts()
 
 
-    #create a new adaptive inspection instance
-    insp1=adaptiveInspect.inspect(scT1.scInterface)
+                            insp1.initTopHist()
 
-    insp1.suspectNodes.append(random.randint(0,size-1))
-    for i in range(size):
-        insp1.costs[i]=0.1
+                            found=False
+                            i=0
+                            while found is not True:
+                                i=i+1
+                                #if ((i % 1) == 0):   #this should be in tweakNodes?
+                                #    insp1.randomCosts()
+                                    #print(insp1.costs)
+                                insp1.monitorTopology()
+                                if v==True:
+                                    insp1.analyseValue()
+                                plan=insp1.planInspection()
 
-    insp1.randomCosts()
-    insp1.initTopHist()
-    found=False
-    i=0
-    while found is not True:
-        i=i+1
-        #if ((i % 1) == 0):
-        #    insp1.randomCosts()
-            #print(insp1.costs)
-        insp1.monitorTopology()
-        insp1.analyseValue()
-        plan=insp1.planInspection()
-        insp1.executeInspection(plan,True)
-        if insp1.found==insp1.suspectNodes:
-            found=True
-        #print(insp1.found)
-        #print(insp1.suspectNodes)
-        #print(insp1.values)
-        insp1.tweakNodes(0.1)
-        #if i > size: break
-    print(i)
-    result=result+i
 
-print(result/runs)
+                                insp1.executeInspection(plan,True)
+                                if insp1.found==insp1.suspectNodes:
+                                    found=True
+                                insp1.tweakNodes(0.1)
+                            #print(i)
+                            dataSet.write
+                            result=result+i
+                            dataSet.write(",%d"%i)
+                        #dataSet.write(",%d"%result/runs)
+                        avgs.write("%d,%s,%s,%f,%s,%d\n" % (size,rCosts,v,nTweak,aFlag,result/runs))
+                        print(result/runs)#
+dataSet.close()
+avgs.close()
