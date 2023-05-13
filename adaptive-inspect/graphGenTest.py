cc4d193f6b6292692bca0c0fb3e868b923861340
tomwelsh <34302535+tomwelsh@users.noreply.github.com>
2023-05-13 06:55:56 +0100

    Update graphGenTest.py

--- adaptive-inspect/graphGenTest.py
+++ adaptive-inspect/graphGenTest.py
@@ -27,66 +27,57 @@ for run in range(runs):
 avgs.write("NetSize,RandomCosts,MonitorFlag,NodeTweak,AdjacencyFlag,CalcCentr,contCost,Avg")
 calcCent=[True,False]
 
-for nTweak in nodeTweak:
-    for rCosts in randomCosts:
-        for calcC in calcCent:
-            for contCost in container:   #increase or not in contCost
-                for adj in adjr:  #centrality flag
-                    for m in monitor:
-                        for size in netSizes:
-                            dataSet.write("\n%d,%s,%s,%f,%d,%s,%f" % (size,rCosts,m,nTweak,adj,calcC,contCost))
-                            dataSet.flush()
-                            results=[]
-                            result=0
-                            for j in range(runs):
-                                scT1=modelTopology.scTopology()
-                                #generate a new graph
-                                scT1.scInterface.genConnect(size)
-                                scT1.scInterface.genContainer()
-                            #    scT1.scInterface.drawGraph()
-                                #scT1.scInterface.drawGraph()
-                                #create a new adaptive inspection instance
-                                insp1=adaptiveInspect.inspect(scT1.scInterface)
+for nTweak, rCosts, calcC, contCost, adj, mon, size in zip(nodeTweak,randomCosts,calcCent,container,adjr,monitor,netSizes):
+    dataSet.write("\n%d,%s,%s,%f,%d,%s,%f" % (size,rCosts,mon,nTweak,adj,calcC,contCost))
+    dataSet.flush()
+    results=[]
+    result=0
+    for j in range(runs):
+        scT1=modelTopology.scTopology()
+        #generate a new graph
+        scT1.scInterface.genConnect(size)
+        scT1.scInterface.genContainer()
+        #create a new adaptive inspection instance
+        insp1=adaptiveInspect.inspect(scT1.scInterface)
 
-                                insp1.suspectNodes.append(random.choice(list(scT1.scInterface.sc.nodes())))
-                                if rCosts==1:
-                                    insp1.randomCosts('asd  ')
-                                else:
-                                    for n in scT1.scInterface.sc.nodes:
-                                        insp1.costs[n]=rCosts
-                                insp1.contCostM=contCost
+        insp1.suspectNodes.append(random.choice(list(scT1.scInterface.sc.nodes())))
+        if rCosts==1:
+            insp1.randomCosts('asd  ')
+        else:
+            for n in scT1.scInterface.sc.nodes:
+                insp1.costs[n]=rCosts
+        insp1.contCostM=contCost
 
-                                insp1.initTopHist()
+        insp1.initTopHist()
 
-                                found=False
-                                i=0
-                                while found is not True:
-                                    i=i+1
-                                    if m==True:
-                                        insp1.monitorTopology()
-                                    if calcC==True:
-                                        insp1.analyseValue(adj)
+        found=False
+        i=0
+        while not found:
+            i=i+1
+            if mon==True:
+                insp1.monitorTopology()
+            if calcC==True:
+                insp1.analyseValue(adj)
 
 
-                                    plan=insp1.planInspection()
-                                    insp1.executeInspection(plan,adj)
+            plan=insp1.planInspection()
+            insp1.executeInspection(plan,adj)
 
-                                    if insp1.found==insp1.suspectNodes:
-                                        found=True
-                                    insp1.tweakNodes(nTweak)
+            if insp1.found==insp1.suspectNodes:
+                found=True
+            insp1.tweakNodes(nTweak)
 
 
-                                    if i >= (size*timeout):
-                                        i=size*timeout
-                                        break
-                                    #print(i)
+            if i >= (size*timeout):
+                i=size*timeout
+                break
+            #print(i)
 
-                                dataSet.write
-                                result=result+i
-                                dataSet.write(",%d"%i)
-                                #dataSet.write(",%d"%result/runs)
-                            avgs.write("\n%d,%s,%s,%f,%d,%s,%f,%d" % (size,rCosts,m,nTweak,adj,calcC,contCost,result/runs))
-                            avgs.flush()
-                        #    print(result/runs)#
+        dataSet.write
+        result=result+i
+        dataSet.write(",%d"%i)
+        #dataSet.write(",%d"%result/runs)
+    avgs.write("\n%d,%s,%s,%f,%d,%s,%f,%d" % (size,rCosts,mon,nTweak,adj,calcC,contCost,result/runs))
+    avgs.flush()
 dataSet.close()
 avgs.close()
